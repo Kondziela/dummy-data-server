065ed730d471e3e07a09cc6cd3a681fc3b731765
akondziela <artur.kondziela@gmail.com>
2020-02-28 09:23:57 +0100

    Add kpi data

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ server.use('/personal', jsonServer.router('dummy_data/data/personal.json'))
 server.use('/interactions', jsonServer.router('dummy_data/data/interactions.json'))
 server.use('/insurance', jsonServer.router('dummy_data/data/insurance.json'))
 server.use('/dashboard', jsonServer.router('dummy_data/data/dashboard.json'))
+server.use('/kpi_report', jsonServer.router('dummy_data/data/kpi_report.json'))
 
 // routing for dummy data from JSON files
 fs.readFile(path.join('dummy_data', 'restpoint_def.json'), (err, data) => {
